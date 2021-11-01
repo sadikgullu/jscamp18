1196bac7e3500f0d3e27b30f6822d97fff50b818
sadikgullu <cptzrflm@gmail.com>
2021-11-02 00:36:22 +0300

    1 Kasım Ders Notu

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 console.log("merhaba codlama.io")
 
+//js type safe değildir
 let dolarBugun = 9.30
 
 let dolarDun = 9.20
-
+dolarDun = "9.20"
 {
     let dolarDun = 9.10
 }
+
 console.log(dolarDun)
 
 const euroDun = 11.2
